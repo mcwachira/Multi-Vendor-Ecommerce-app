840251735eef603488532b8b11ec5f488c25c10c
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-04 11:45:59 +0300

    Chore: Created a dynamic Header

---
+++ src/components/backoffice/Heading.tsx
@@ -0,0 +1,10 @@
+interface HeadingProps {
+    title:string
+}
+const Heading = ({title}:HeadingProps) => {return (
+<h2 className=" text-2xl font-semibold text-slate-50">
+    {title}
+</h2>
+)}
+
+ export default Heading
