37a3c1f56471ea15a7f4c38abc36340e2f86ae65
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-04 15:11:20 +0300

    Mae changes to the app and  added some new packages

--- src/app/(back-office)/dashboard/page.tsx
+++ src/app/(back-office)/dashboard/page.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import Heading from "@/components/backoffice/Heading";
+import LargeCards from "@/components/backoffice/LargeCards";
+import SmallCards from "@/components/backoffice/SmallCards";
+import DashboardCharts from "@/components/backoffice/DashboardCharts";
 
-function Dashboard(props) {
+function Dashboard() {
     return (
-        <div></div>
+        <div>
+
+            <Heading title="Dashboard Overview"/>
+            
+        {/*    large Cards*/}
+            <LargeCards/>
+            
+        {/*    small
+        */}
+
+            <SmallCards/>
+
+        {/*    Charts*/}
+            <DashboardCharts/>
+        </div>
     );
 }
 
--- src/app/(back-office)/layout.tsx
+++ src/app/(back-office)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Sidebar from "@/components/backoffice/Sidebar";
+import Navbar from "@/components/backoffice/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,16 +17,14 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
- <div className='flex'>
+ <div className='flex '>
    {/*sidebar*/}
 <Sidebar/>
 
      <div className='w-full'>
-         <h2>
-             nAVBAR
-         </h2>
+     <Navbar/>
 
-         <main>
+         <main className="p-8 bg-slate-900 text-slate-50 min-h-screen mt-16  ">
              {children}
          </main>
      </div>
