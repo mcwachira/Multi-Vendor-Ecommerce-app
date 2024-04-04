c26fdce60c43cb898fbf7f0c30a2636eb2283a1f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-04 15:10:45 +0300

    Chore:Created Dashboard Component to showcase my two charts

---
+++ src/components/backoffice/DashboardCharts.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import WeeklySalesChart from "@/components/backoffice/WeeklySalesChart";
+import BestSellingProductsCharts from "@/components/backoffice/BestSellingProductsCharts";
+
+function DashboardCharts(props) {
+    return (
+   <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 ">
+<WeeklySalesChart/>
+       <BestSellingProductsCharts/>
+   </div>
+    )
+}
+
+export default DashboardCharts;
