d087abd61307521be81317ef8d0775b766944e7f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-04-05 11:50:55 +0300

    Chore:Made some changes to the app

--- src/components/backoffice/Navbar.tsx
+++ src/components/backoffice/Navbar.tsx
@@ -3,7 +3,7 @@ import {AlignJustify, Bell, Sun, User} from "lucide-react";
 
 function Navbar() {
     return (
-        <div className='flex items-center justify-between bg-slate-800 text-slate-50 h-16 px-8 py-4  fixed top-0 ml-60 w-full'>
+        <div className='w-full flex items-center justify-between bg-slate-800 text-slate-50 h-16 px-8 py-4  fixed top-0 left-60 right-0 '>
 
             <button>
                 <AlignJustify/>
